e92f50e19bba081b991fbdd4a6321752620fa366
Zack Dorward <zdorward1@gmail.com>
2023-07-07 01:31:55 -0600

    got prettier working i think

--- components/Tile.vue
+++ components/Tile.vue
@@ -1,4 +1,6 @@
-<template>hello world</template>
+<template>
+    <div class="container">test</div>
+</template>
 
 <script setup lang="ts">
 const a = 1
@@ -7,3 +9,5 @@ const b = () => {
     return 1
 }
 </script>
+
+<style scoped></style>
